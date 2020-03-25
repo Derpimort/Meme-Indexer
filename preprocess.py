50d91e99055c96fde1fd06777962e7c0274cf186
Derpimort <jatinsaini580@gmail.com>
2020-03-25 20:30:09 +0530

    Added output extension selection

--- preprocess.py
+++ preprocess.py
@@ -40,17 +40,18 @@ def getNumberOfFiles(directory):
 				extensions[ext]+=1
 	return extensions
 
-def printData(src_dir, dst_dir,ext_dict):
+def printData(src_dir, dst_dir,ext_dict, output_suffix):
 	print(f"Source: {src_dir}")
 	print(f"Destination: {dst_dir}")
+	print(f"Output files suffix: {output_suffix}")
 	
-	print("Number of files in source directory:")
+	print("\nNumber of files in source directory:")
 	for extension in ext_dict:
 		print(f"{extension} : {total_files[extension]}")
 	
 	print()
 
-def save_gif_selective(fname,save_dir):
+def save_gif_selective(fname,save_dir, target_size, output_suffix=".jpg"):
 	im = Image.open(fname)
 	fname=fname.split("/")[-1][:-4]
 	save_dir=os.path.join(save_dir,fname)
@@ -59,7 +60,13 @@ def save_gif_selective(fname,save_dir):
 	index=0
 	for frame in ImageSequence.Iterator(im):
 		if index in req_frames:
-			frame.save(save_dir+"_%04d.png" % index)
+			fname=save_dir+"_%04d.png" % index
+			frame.save(fname)
+			dfname="".join(fname.split(".")[:-1])+output_suffix
+			im=cv2.imread(fname)
+			im=cv2.resize(im, target_size, interpolation=cv2.INTER_CUBIC)
+			cv2.imwrite(dfname ,im)
+			os.remove(fname)
 		index += 1
 
 def save_gif_all(fname,save_dir):
@@ -72,27 +79,28 @@ def save_gif_all(fname,save_dir):
 		frame.save(save_dir+"/%04d.png" % index)
 		index += 1
 
-def preProcess(src_dir, dst_dir, target_size=(224,224), ext_dict=None):
+def preProcess(src_dir, dst_dir, target_size=(224,224), ext_dict=None, output_suffix=".jpg"):
 	
 	if ext_dict is None:
 		ext_dict=getNumberOfFiles(src_dir)
-		printData(src_dir, dst_dir, ext_dict)
+		printData(src_dir, dst_dir, ext_dict, output_suffix)
 	total_files=sum(ext_dict.values())
 	progress=ProgressBar(total_files)
 	failed=[]
 	for dirname, _, filenames in os.walk(src_dir):
 		for filename in filenames:
 			try:
-				if (filename.endswith("gif")):
-					save_gif_selective(os.path.join(dirname, filename), dst_dir)
+				if (filename.endswith(".gif")):
+					save_gif_selective(os.path.join(dirname, filename), dst_dir, target_size, output_suffix)
 				else:
+					dfname="".join(filename.split(".")[:-1])+output_suffix
 					im=cv2.imread(os.path.join(dirname, filename))
 					im=cv2.resize(im, target_size, interpolation=cv2.INTER_CUBIC)
-					cv2.imwrite(os.path.join(dst_dir, filename),im)
+					cv2.imwrite(os.path.join(dst_dir,dfname) ,im)
 				progress.increment()
 			except Exception as e:
 				failed.append((os.path.join(dirname,filename),e))
-				
+
 	print("\nSuccessfully resize %d files"%progress.curr_counter)
 	print("%d Failed "%len(failed))
 	for i,e in failed:
@@ -114,6 +122,12 @@ if __name__ == "__main__":
 		"-d",
 		help="Destination directory, default is current working directory")
 	
+	parser.add_argument(
+		"--output-suffix",
+		"-e",
+		help="Specify extension for output files, default=.jpg",
+		default=".jpg",
+		choices=[".png",".jpg"])
 	args=parser.parse_args()
 	target_size=(224,224)
 	try:
@@ -126,17 +140,16 @@ if __name__ == "__main__":
 	if args.dst is not None:
 		destination_dir=os.path.abspath(args.dst)
 	destination_dir=os.path.join(destination_dir,"Preprocessed_imgs")
-	
-	
+	output_suffix=args.output_suffix
 	total_files=getNumberOfFiles(source_dir)
-	printData(source_dir, destination_dir, total_files)
+	printData(source_dir, destination_dir, total_files, output_suffix)
 	
 	yes=["y","yes",""]
 	
-	proceed=input("Proceed [y]/n:").lower()
+	proceed=input("\n\nProceed [y]/n:").lower()
 	
 	if proceed in yes:
 		checkDir(destination_dir)
-		preProcess(source_dir, destination_dir, target_size,total_files)
+		preProcess(source_dir, destination_dir, target_size,total_files, output_suffix)
 	else:
 		print("Aborted")
