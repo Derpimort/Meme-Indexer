932178325dfd66779aff2bd21d6d94aa1dee5c0f
Derpimort <jatinsaini580@gmail.com>
2020-06-13 10:50:19 +0530

    Directory traversal complete.

--- app/src/main/java/com/legendbois/memeindexer/ui/main/IndexBuilderFragment.kt
+++ app/src/main/java/com/legendbois/memeindexer/ui/main/IndexBuilderFragment.kt
@@ -2,7 +2,10 @@ package com.legendbois.memeindexer.ui.main
 
 import android.app.Activity
 import android.content.Intent
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -10,6 +13,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.legendbois.memeindexer.R
+import java.io.Closeable
+import java.util.*
+
 
 class IndexBuilderFragment: Fragment(), View.OnClickListener {
     companion object{
@@ -39,11 +45,90 @@ class IndexBuilderFragment: Fragment(), View.OnClickListener {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK){
-            if(requestCode == DIRECTORY_REQUEST_CODE){
-                Log.d(TAG, data?.data.toString())
-            }
+        if (resultCode == Activity.RESULT_OK) {
+            if (data != null) {
+                if (requestCode == DIRECTORY_REQUEST_CODE) {
+                    val parentUri = data.data
+                    traverseDirectoryEntries(parentUri)
+                    Log.d(TAG, data.data.toString())
+                }
 
+            }
+        }
+    }
+
+    //Thanks to https://stackoverflow.com/questions/41096332/issues-traversing-through-directory-hierarchy-with-android-storage-access-framew
+    fun traverseDirectoryEntries(rootUri: Uri?){
+        val contentResolver = activity!!.contentResolver
+        var childrenUri: Uri = try {
+            //for childs and sub child dirs
+            DocumentsContract.buildChildDocumentsUriUsingTree(
+                rootUri,
+                DocumentsContract.getDocumentId(rootUri)
+            )
+        } catch (e: java.lang.Exception) {
+            // for parent dir
+            DocumentsContract.buildChildDocumentsUriUsingTree(
+                rootUri,
+                DocumentsContract.getTreeDocumentId(rootUri)
+            )
+        }
+
+        // Keep track of our directory hierarchy
+        val dirNodes: MutableList<Uri> = LinkedList()
+        dirNodes.add(childrenUri)
+        while (dirNodes.isNotEmpty()) {
+            childrenUri = dirNodes.removeAt(0) // get the item from top
+            Log.d(TAG, "node uri:  $childrenUri")
+            val c: Cursor? = contentResolver.query(
+                childrenUri,
+                arrayOf(
+                    DocumentsContract.Document.COLUMN_DOCUMENT_ID,
+                    DocumentsContract.Document.COLUMN_DISPLAY_NAME,
+                    DocumentsContract.Document.COLUMN_MIME_TYPE
+                ),
+                null,
+                null,
+                null
+            )
+            try {
+                if (c!=null) {
+                    while (c.moveToNext()) {
+                        val docId: String = c.getString(0)
+                        val name: String = c.getString(1)
+                        val mime: String = c.getString(2)
+                        Log.d(
+                            TAG,
+                            "docId: $id, name: $name, mime: $mime"
+                        )
+                        if (isDirectory(mime)) {
+                            val newNode: Uri =
+                                DocumentsContract.buildChildDocumentsUriUsingTree(rootUri, docId)
+                            dirNodes.add(newNode)
+                        }
+                    }
+                }
+            } finally {
+                closeQuietly(c)
+            }
+        }
+    }
+
+    // Util method to check if the mime type is a directory
+    private fun isDirectory(mimeType: String): Boolean {
+        return DocumentsContract.Document.MIME_TYPE_DIR == mimeType
+    }
+
+    // Util method to close a closeable
+    private fun closeQuietly(closeable: Closeable?) {
+        if (closeable != null) {
+            try {
+                closeable.close()
+            } catch (re: RuntimeException) {
+                throw re
+            } catch (ignore: Exception) {
+                // ignore exception
+            }
         }
     }
 }
