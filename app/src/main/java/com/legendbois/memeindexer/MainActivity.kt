2712470068c2e26fc51b7b107304dd808789e235
Derpimort <jatinsaini580@gmail.com>
2020-06-13 11:17:42 +0530

    Add permission request dialog.

--- app/src/main/java/com/legendbois/memeindexer/MainActivity.kt
+++ app/src/main/java/com/legendbois/memeindexer/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.legendbois.memeindexer.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -21,10 +22,31 @@ class MainActivity : AppCompatActivity() {
         val tabs: TabLayout = findViewById(R.id.tabs)
         tabs.setupWithViewPager(viewPager)
         val fab: FloatingActionButton = findViewById(R.id.fab)
+        if (!PermissionHelper.hasPermissions(this)) {
+            //Log.d("APSIT","Permission checker")
+            PermissionHelper.getPermissions(this)
 
+        }
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PermissionHelper.PERMISSION_CODE) {
+            if (!PermissionHelper.hasPermissions(this)) {
+                Toast.makeText(this, "App needs camera and storage permissions", Toast.LENGTH_LONG) .show()
+                finish()
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+
+
     }
 }
