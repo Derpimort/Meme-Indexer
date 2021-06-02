a0554968fa53b430c8cc90ac5aac6556127cd838
Derpimort <jatinsaini580@gmail.com>
2021-06-03 00:41:43 +0530

    UPDATE: add path on textview click

--- app/src/main/java/com/legendbois/memeindexer/ui/main/IndexBuilderFragment.kt
+++ app/src/main/java/com/legendbois/memeindexer/ui/main/IndexBuilderFragment.kt
@@ -10,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
@@ -59,13 +60,23 @@ class IndexBuilderFragment: Fragment(), View.OnClickListener {
         rootView = inflater.inflate(R.layout.indexbuilder_frag, container, false)
         val button: Button = rootView.findViewById(R.id.indexbuilder_button)
         val pathButton: ImageButton = rootView.findViewById(R.id.indexbuilder_path_button)
+        val pathText: TextView = rootView.findViewById(R.id.indexbuilder_path)
+
         button.setOnClickListener(this)
+
         pathButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
-            intent.addCategory(Intent.CATEGORY_DEFAULT)
-            startActivityForResult(Intent.createChooser(intent, "Choose directory"), DIRECTORY_REQUEST_CODE)
+            addPath()
+        }
+        pathText.setOnClickListener {
+            addPath()
         }
         return rootView
+    }
+
+    fun addPath(){
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT_TREE)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        startActivityForResult(Intent.createChooser(intent, "Choose directory"), DIRECTORY_REQUEST_CODE)
     }
 
     override fun onClick(v: View?) {
