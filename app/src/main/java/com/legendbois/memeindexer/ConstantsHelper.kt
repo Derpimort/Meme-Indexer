b786fa0b737ae9d5745a881117c03a52644db0ba
Derpimort <jatinsaini580@gmail.com>
2021-09-06 01:12:43 +0530

    UPDATE: constants const modifier

--- app/src/main/java/com/legendbois/memeindexer/ConstantsHelper.kt
+++ app/src/main/java/com/legendbois/memeindexer/ConstantsHelper.kt
@@ -1,12 +1,14 @@
 package com.legendbois.memeindexer
 
 object ConstantsHelper {
-    val defaultText = "5PAC38AR_UN1ND3X3D_5P3C1F13R"
-    val defaultFailedText = "5PAC38AR_FA1L3D_5P3C1F13R"
-    val PERIODIC_WORKREQ_TAG = "MemeIndexer-Daily-Scan"
-    val WORKMANAGER_UID = "MemeIndexer-Daily-Scan-Worker"
-    val ONETIME_WORKMANAGER_UID = "MemeIndexer-Scan-Worker"
-    val ONETIME_WORKREQ_TAG = "MemeIndexer-Onetime-Scan"
+    const val defaultText = "5PAC38AR_UN1ND3X3D_5P3C1F13R"
+    const val defaultFailedText = "5PAC38AR_FA1L3D_5P3C1F13R"
+
+    const val PERIODIC_WORKREQ_TAG = "MemeIndexer-Daily-Scan"
+    const val WORKMANAGER_UID = "MemeIndexer-Daily-Scan-Worker"
+    const val ONETIME_WORKMANAGER_UID = "MemeIndexer-Scan-Worker"
+    const val ONETIME_WORKREQ_TAG = "MemeIndexer-Onetime-Scan"
+
     val THEMES = mapOf(
         "Legacy" to R.style.AppTheme_Legacy,
         "Dark" to R.style.AppTheme_Dark,
